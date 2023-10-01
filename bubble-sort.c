21477d49e33080803bfc14db70f065ab70804b6d
David Ifeoluwa Agbemuko <davidagbemuko.ad@gmail.com>
2023-10-01 07:47:50 +0100

    set up code for descending and ascending sort functionality

--- bubble-sort.c
+++ bubble-sort.c
@@ -9,6 +9,7 @@ int main (void)
 	int           i, j, mode, tmp, sum = 0;
 	float         mean, median;
 	int           array_size;
+	char          order;
 
 	prn_instructions();
 
@@ -17,6 +18,7 @@ int main (void)
 	printf(">> ");
 	scanf("%d", &array_size);
 
+	// ASK FOR THE ARRAY ELEMENTS
 	printf("\n\n Please input %d numbers >>  ", array_size);
 	int          array[array_size];
 	for (i = 0; i < array_size; ++i) {
@@ -24,7 +26,19 @@ int main (void)
 		sum += array[i];		
 	}
 
+	// CONFIRM IN WHAT ORDER IT IS NEEDED TO BE SORTED
+	printf("\nIn what order do you want your array sorted ('a' for ascending, 'd' for descending)>> ");
+	scanf(" %c", &order);
 
+	if (order == 'a') {
+		printf("\nYou have chosen the ascending order.");
+		//sort_acending(array); //A FUNCTION THAT SORTS IN ASCENDING
+		}
+	else if (order == 'd') {
+		printf("\nYou have chosen the descending order.");
+		//sort_descending(array); //A FUNCTION THAT SORTS IN DESCENDING
+		}
+			
 	return 0;
 }
 
